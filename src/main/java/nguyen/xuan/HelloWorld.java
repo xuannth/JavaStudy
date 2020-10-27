a546027a1bd26fb6e344b5f72f43d3f8e2a92b45
xuan <hongxuank50a2@gmail.com>
2020-10-27 13:17:13 +0700

    Bai tap ve convert chuoi sang mang

--- src/main/java/nguyen/xuan/HelloWorld.java
+++ src/main/java/nguyen/xuan/HelloWorld.java
@@ -3,10 +3,54 @@ package nguyen.xuan;
 public class HelloWorld {
 
 	public static void main (String args[]) {
-		String[] arr = {"A","B","C","D","E"};
-		System.out.println("Cac gia tri trong mang string la: ");
-		for (int i = 0; i < arr.length; i ++) {
-			System.out.println(arr[i]); 
+		String s = new String("Hello World");
+		System.out.println("Doi chu o thanh chu O: " + s.replace('o', 'O'));
+		
+		char[] arr1= s.toCharArray();
+		int len = arr1.length;
+		
+		System.out.println("Chuoi truoc khi dao nguoc la: ");
+		for (int i = 0; i<len; i ++ ) {
+			System.out.print(arr1[i]); 
 		}
+		
+		char[] arr2 = new char[len];
+		
+		System.out.println("\nChuoi sau khi dao nguoc la: ");
+		String result = "";
+		
+		for (int i = len - 1; i >= 0; i --) {
+				arr2[len - 1 - i] = arr1[i];
+				result += arr2[len - 1 -i];
+		}	
+		System.out.println(result);
+		
+		String str = s.toLowerCase();
+		char[] arr3= str.toCharArray();
+		int[] arr = new int[len];
+		
+		System.out.println("Chuoi truoc khi sap xep: ");
+		
+		for (int i = 0; i < len; i++) {
+			arr[i] = (int)arr3[i];
+			System.out.print((char)arr[i]);
+		}
+		
+		int temp = arr[0];
+		for (int i = 0; i < len-1; i++) {
+			for (int j = i + 1; j < len; j++) {
+	    		if (arr[i] > arr[j]) {
+	    			temp = arr[j];
+	    			arr[j] = arr[i];
+	    			arr[i] = temp ;
+	    		}
+	    	}
+		}
+		
+		System.out.println("\nSap xep chuoi theo Alphabet: ");
+		for (int i = 0; i < len; i++) {
+			System.out.print((char)arr[i]);
+		}
+		
 	}
 }
